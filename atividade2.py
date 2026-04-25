005152a3b0f8d12d8830e61807386b069e39adc2
vladimirsejas <vladimirqueirozsejas@gmail.com>
2026-04-25 01:34:53 -0300

    aplica decorator de medição de tempo na atividade 2

--- atividade2.py
+++ atividade2.py
@@ -1,26 +1,24 @@
-from lgpd import LGPD, engine  # importa a função LGPD e a conexão
+from lgpd import LGPD, engine
+from atividade4 import medir_tempo
 import pandas as pd
 from sqlalchemy import text
 
+@medir_tempo
 def exportar_por_ano():
     users = []
-    # Consulta todos os registros do banco
     with engine.connect() as conn:
         result = conn.execute(text("SELECT * FROM usuarios;"))
         for row in result:
-            row = LGPD(row)  # aplica anonimização
+            row = LGPD(row)
             users.append(row)
 
-    # Cria DataFrame com os registros anonimizados
     df = pd.DataFrame(users, columns=[
-        "id", "nome", "cpf", "email", "telefone", 
+        "id", "nome", "cpf", "email", "telefone",
         "data_nascimento", "created_on", "updated_on"
     ])
 
-    # Converte a coluna para datetime (garante que .dt.year funcione)
     df["data_nascimento"] = pd.to_datetime(df["data_nascimento"])
 
-    # Exporta por ano de nascimento
     for ano in df["data_nascimento"].dt.year.unique():
         df_ano = df[df["data_nascimento"].dt.year == ano]
         df_ano.to_csv(f"{ano}.csv", index=False)
